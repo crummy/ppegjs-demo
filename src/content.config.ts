48135b43e92e43e788d8c011238f04da380e3932
Malcolm Crum <mcrum@canva.com>
2024-12-20 14:18:59 +1300

    Examples are more readable stored in this format. Write a parser to read them.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,70 @@
-import {defineCollection, z} from 'astro:content';
-import {glob} from "astro/loaders";
+/**
+ * This file parses our examples from the filesystem and provides them to Astro.
+ *
+ * As a dogfooding exercise, the examples are parsed using our pPEG parser.
+ * Examples are expected to be in src/examples, in this format:
+ *
+ * title: Example Title
+ * ---
+ * (grammar)
+ * ---
+ * (input)
+ */
 
-// TODO: convert to markdown, if astro is happy with multiple files in a single markdown file - easier to do titles too
+import {defineCollection, z} from 'astro:content';
+import fastGlob from "fast-glob";
+import {promises as fs} from "node:fs";
+import {ppeg} from "ppegjs";
+
+const grammar = `
+File = Fields _sep grammar _sep input
+grammar = ~_sep+
+input = ~_sep+
+_sep = '---\\n'
+Fields = (comment / field)+
+field = key ':' _ value  '\\r'? '\\n'?
+comment = '#' ~[\\r\\n]* '\\r'? '\\n'?
+key = [a-zA-Z0-9_]+
+value = ~[\\n]+
+_ = [ \\t]*
+`
+
+type Field = [
+    "field",
+    ["key", string],
+    ["value", string]
+]
+
+type PTree = [
+    "File", [
+        ["Fields", Field[]],
+        ["grammar", string],
+        ["input", string],
+    ]
+]
+
+const parser = ppeg.compile(grammar);
+
 export const examples = defineCollection({
-    loader: glob({pattern: "src/examples/*.json"}),
+    loader: async () => {
+        // We shouldn't need fastGlob here, better to use import.meta.glob. But there's a bug:
+        // https://github.com/withastro/astro/issues/12689
+        const files = await fastGlob("src/examples/*.txt");
+        return await Promise.all(files.map(async f => {
+            const contents = await fs.readFile(f, "utf-8")
+            const {ptree}: { ptree: PTree } = parser.parse(contents)
+            const fields = Object.fromEntries(ptree[1][0][1].map(field => field[1]).map(([key, value]) => [key[1], value[1]]))
+            const grammar = ptree[1][1][1]
+            const input = ptree[1][2][1]
+            return {
+                ...fields,
+                id: fields.title,
+                highlighted: fields.highlighted == "true",
+                grammar,
+                input
+            }
+        }));
+    },
     // This schema ensures that the data read in the collection above is valid, by parsing it with Zod
     schema: z.object({
         title: z.string(),
